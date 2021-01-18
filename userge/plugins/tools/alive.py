d86ba3ab69892e05523e7dc6c82493eb5aa3c3e5
csr87 <74970184+csr87@users.noreply.github.com>
2021-01-18 08:50:14 +0700

    Update alive.py

--- userge/plugins/tools/alive.py
+++ userge/plugins/tools/alive.py
@@ -27,7 +27,7 @@ _LOG = logging.getLogger(__name__)
 _IS_TELEGRAPH = False
 _IS_STICKER = False
 
-_DEFAULT = "https://t.me/theUserge/31"
+_DEFAULT = "https://telegra.ph/file/17ec02e2ca7d4d5d165ee.jpg"
 _CHAT, _MSG_ID = None, None
 _LOGO_ID = None
 
@@ -65,20 +65,24 @@ def _get_mode() -> str:
 def _get_alive_text_and_markup(message: Message) -> Tuple[str, Optional[InlineKeyboardMarkup]]:
     markup = None
     output = f"""
-**⏱ Uptime** : `{userge.uptime}`
-**💡 Version** : `{get_version()}`
-**⚙️ Mode** : `{_get_mode().upper()}`
-
-• **Sudo**: `{_parse_arg(Config.SUDO_ENABLED)}`
-• **Pm-Guard**: `{_parse_arg(not Config.ALLOW_ALL_PMS)}`
+**⏱ UmurBot** : `{userge.uptime}`
+**💡 VersiBot** : `{get_version()}`
+**⚙️ ModeBot** : `{_get_mode().upper()}`
+**❤️ ModeCinta** : `active`
+• **Digerakin**: `{_parse_arg(Config.SUDO_ENABLED)}`
+• **Perlindungan-Pm**: `{_parse_arg(not Config.ALLOW_ALL_PMS)}`
 • **Anti-Spam**: `{_parse_arg(Config.ANTISPAM_SENTRY)}`"""
     if Config.HEROKU_APP:
         output += f"\n• **Dyno-saver**: `{_parse_arg(Config.RUN_DYNO_SAVER)}`"
     output += f"""
 • **Unofficial**: `{_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}`
 
-    **__Python__**: `{versions.__python_version__}`
-    **__Pyrogram__**: `{versions.__pyro_version__}`"""
+    **🐍Versi UlarPiton**:`{versions.__python_version__}`
+    **🐛Versi AdekPiton**: `{versions.__pyro_version__}`
+    **🤖Versi Bot**: `beta`
+    
+    **Order SSH Premium hubungi @caesaranggaaa**"""
+  
     if not message.client.is_bot:
         output += f"""\n
 🎖 **{versions.__license__}** | 👥 **{versions.__copyright__}** | 🧪 **[Repo]({Config.UPSTREAM_REPO})**
@@ -87,7 +91,7 @@ def _get_alive_text_and_markup(message: Message) -> Tuple[str, Optional[InlineKe
         copy_ = "https://github.com/UsergeTeam/Userge/blob/master/LICENSE"
         markup = InlineKeyboardMarkup([
             [
-                InlineKeyboardButton(text="👥 UsergeTeam", url="https://github.com/UsergeTeam"),
+                InlineKeyboardButton(text="👥 CsrTeam", url="http://t.me/caesaranggaaa"),
                 InlineKeyboardButton(text="🧪 Repo", url=Config.UPSTREAM_REPO)
             ],
             [InlineKeyboardButton(text="🎖 GNU GPL v3.0", url=copy_)]
